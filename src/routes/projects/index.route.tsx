92319ed1bd0e2452fe8b8a7d12e5b0990d3fcb11
Pietro <info@pietrobongiovanni.com>
2024-12-08 13:59:30 +0100

    x

--- src/routes/projects/index.route.tsx
+++ src/routes/projects/index.route.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 import { useQuery } from '@tanstack/react-query'
 import { invoke_tauri_command } from '@/lib/utils'
 import { ColumnDef } from '@tanstack/react-table'
